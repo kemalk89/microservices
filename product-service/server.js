30b02b381e240a4e59f68d5ba031c39635c5de43
kemal <k.kemal.1989@gmail.com>
2022-11-12 07:35:49 +0100

    Connecting to mongodb.

--- product-service/server.js
+++ product-service/server.js
@@ -1,6 +1,22 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3002;
+
+async function connectToMongoDB(mongoHost, mongoDbPort, mongoDb, username, password) {
+  const uri = `mongodb://${username}:${password}@${mongoHost}:${mongoDbPort}/${mongoDb}?authSource=admin`;
+
+  const client = new MongoClient(uri);
+
+  try {
+    console.log('Trying to connect to mongodb service.');
+    await client.connect();
+    console.log('Successfully connected to mongodb.');
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 app.get('/api/products', (req, res) => {
   const products = [
@@ -20,6 +36,13 @@ app.get('/', (req, res) => {
   res.send('Hello from Product Service!!!!');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  const mongoDbHost = process.env['MONGODB_HOST'];
+  const mongoDbPort = process.env['MONGODB_PORT'];
+  const mongoDb = process.env['MONGODB_DB'];
+  const mongoDbUser = process.env['MONGODB_DB_USER'];
+  const mongoDbUserPw = process.env['MONGODB_DB_USER_PW'];
+  await connectToMongoDB(mongoDbHost, mongoDbPort, mongoDb, mongoDbUser, mongoDbUserPw);
+
   console.log(`Product service API listening on port ${port}`);
 });
